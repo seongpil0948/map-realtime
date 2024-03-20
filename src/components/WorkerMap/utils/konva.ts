adcf242bb7702ff6ed5385ac969acefdf3765b5a
최성필 <choeseongpil@choeseongpils-MacBook-Air.local>
2024-03-20 10:19:48 +0900

    feat: object label

--- src/components/WorkerMap/utils/konva.ts
+++ src/components/WorkerMap/utils/konva.ts
@@ -1,5 +1,6 @@
 import { Ref } from "vue"
-import { Node, Stage, Vector2D } from "../../../types"
+import { Node, Stage, TextConfig, Vector2D } from "../../../types"
+import factory from "./factory"
 
 const isValidStage = (stage: any) => {
   // TODO(임규리): 무조건 stageRef만 들어올 수 있도록 
@@ -33,3 +34,43 @@ export function moveNode(node: Node, x: number, y: number) {
   })
 }
 
+
+export function generateObjectLabel(
+  {
+    konvaEl,
+    stageRef,
+    place,
+    text,
+    distance = 10
+  }: {
+    konvaEl: any;
+    stageRef: Ref<any>;
+    place: 'top' | 'bottom' | 'left' | 'right';
+    text: (node: Node) => string;
+    distance?: number
+  }): TextConfig {
+  const node = konvaEl.getNode() as Node
+  const stage = extractStage(stageRef)
+  const rect = node.getClientRect({ relativeTo: stage })
+  console.log("node: ", node)
+  const tConfig: TextConfig = {
+    ...factory.konva.text(),
+    x: rect.x + rect.width / 4,
+    y: rect.y - distance,
+    text: text(node),
+    fill: 'red',
+    align: 'center',
+    fontSize: 8
+  }
+  if (place === 'top') {
+    return tConfig
+  }
+  else if (place === 'bottom') {
+    return {
+      tConfig,
+      x: rect.x + rect.width / 4,
+      y: rect.y + rect.height + distance,
+    }
+  }
+  throw new Error('invalid place')
+}
